624c492b92cb954343e078f6d288f64aae65c843
harshal9876 <63243859+harshal9876@users.noreply.github.com>
2021-01-26 23:06:26 +0530

    Update Assignment1.py

--- Codes/Assignment1.py
+++ Codes/Assignment1.py
@@ -1,91 +1,98 @@
-# Importing Libraries
+# Importing Libraries and fixing plots 
 import numpy as np
-import math 
-from matplotlib import pyplot as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt 
+%matplotlib inline  
+# Change inline to notebook for a 3D interactive view 
+plt.rcParams['figure.figsize'] = (7,5)
+plt.rcParams['figure.dpi']=150
+from mpl_toolkits.mplot3d import Axes3D
+fig=plt.figure()
+ax = fig.add_subplot(111,projection ='3d')
+ax.set_title('Setting up variables')
+ax.set_xlabel('random variable $X_1$')
+ax.set_ylabel('Random vriable $X_2$')
+ax.set_zlabel('$p_{X1,X2}(x1,x2)$')
 
-#Problem 6.2.1
 
-#Taking input for random variables 
+# Generating random points
+mean = 0
+sigma = 1
+x_1 = (np.random.rand(1000,1)-0.5)*8
+x_2 = (np.random.rand(1000,1)-0.5)*8
 
-v = input(" Please input a value for V : ") 
-v=float(v)
-datax = np.linspace(-np.pi,np.pi,10000)
-plot1 = plt.figure(1)
-fx1 = np.sqrt(v)*np.sin(datax)
-plt.plot(datax,fx1)
-plt.grid()
-plt.title('Variation if X1')
-plt.xlabel('Range of x1')
-plt.ylabel('$\sqrt{V}sin(x1)$')
-plot2 = plt.figure(2)
-fx1 = np.sqrt(v)*np.cos(datax)
-plt.plot(datax,fx1)
-plt.grid()
-plt.title('Variation if X2')
-plt.xlabel('Range of x2')
-plt.ylabel('$\sqrt{V}cos(x2)$')
-plot2 = plt.figure(2)
 
-#Calculating Quantile function
+# Function for single variable Gaussian distribution for random variable 'x' , Mean 'mean' and Standard deviation 'sigma' 
 
-datav=np.linspace(-np.sqrt(v),np.sqrt(v),1000)
-plot3=plt.figure(3)
-fx3=np.arcsin(datav/np.sqrt(v)) 
-plt.plot(datav,fx3)
-plt.title('Variation if a vs arcsin viz 2.1')
-plt.xlabel('Range of a')
-plt.ylabel('$arcsin\frac{a}{\sqrt{V}}$')
-plt.grid()
-plot4=plt.figure(4)
-fx4=np.arccos(datav/np.sqrt(v))
-plt.plot(datav,fx4)
-plt.title('Variation if a vs arccos viz 2.2')
-plt.xlabel('Range of a')
-plt.ylabel('$arccos\frac{a}{\sqrt{V}}$')
-plt.grid()
+def normal_distribution(x,mean, sigma):
+  normal_value = 1/(sigma*np.sqrt(2*np.pi))*np.exp(-0.5*((x-mean)/sigma)**2)
+  return normal_value
 
-# Calculating PDF via Quantile function 
+# Function for bivariable Gaussian distribution for random variable 'x_1' and 'x_2'
+# Having respective mean - 'mean_1' and 'mean_2'
+# Having respective standard deviation 'sigma_1' and 'sigma_2'
+# having a covariance of matrix 'cov'
 
-plot6=plt.figure(6)
-fx1=(np.arcsin(datav)+math.pi/2)/math.pi
-plt.plot(datav,fx1)
-plt.title('Variation if CDF of F1(a) vs a')
-plt.xlabel('Range of a')
-plt.ylabel('F1(a)')
-plt.grid()
-plot5=plt.figure(5)
-fx4=(-np.arccos(datav)+math.pi)/math.pi
-plt.plot(datav,fx4)
-plt.title('Variation if CDF of F2(a) vs a')
-plt.xlabel('Range of a')
-plt.ylabel('F2(a)')
-plt.grid()
+def multivar_gaussian_distribution(x_1, x_2, mean_1, mean_2, sigma_1, sigma_2 ,cov):
+    ro = cov[0,1]/(sigma_1*sigma_2)
+    dev_1 = x_1 - mean_1
+    dev_2 = x_2 - mean_2
+    z = (dev_1/sigma_1)**2+(dev_2/sigma_2)**2-((2*ro*dev_1*dev_2)/(sigma_1*sigma_2))
+    result = (1/(2*np.pi*sigma_1*sigma_2*np.sqrt(1-ro**2)))*np.exp((-1*z)/(2*(1-ro**2)))
+    return result
+  
+  
+#Calculating normal distribution for single random variable
+x_1_Normal_distribution = normal_distribution(x_1,mean,sigma)
+x_2_Normal_distribution = normal_distribution(x_2,mean,sigma)
+plt.plot(x_1_Normal_distribution,'ro')
+plt.plot(x_2_Normal_distribution,'bo')
+plt.xlabel('Number of samples')
+plt.ylabel('Gaussian distribution of samples ')
+plt.title('Variation of samples in the gaussian plane ')
+plt.legend(["$ X1 \sim \mathcal{N}(0,\,1) $", "$ X2 \sim \mathcal{N}(0,\,1) $"], loc ="upper right") 
+plt.show()
+
+
+#Calculation of joint distribution
+result=[]
+for x in x_1_Normal_distribution:
+  for y in x_2_Normal_distribution:
+    result.append(x*y)
+result = np.asarray(result)
+
+
+# calculating covariance for random variables
+concat_random_variable = np.concatenate((x_1,x_2),axis=1)
+covariance__random_variable = np.cov(np.transpose(concat_random_variable),bias=True)
+mean_1 = mean_2 = 0
+sigma_1 = sigma_2 = 1
+
+
+#Creating joint random variable distribution 
+dist=[]
+for i in x_1:
+    for j in x_2:
+        dist.append(multivar_gaussian_distribution(i,j,mean_1,mean_2,sigma_1,sigma_2,covariance__random_variable))
+             
+dist=np.asarray(dist)
+
+
 
 #Calculating Joint PDF
+fig=plt.figure()
+ax=fig.add_subplot(111, projection = '3d')
+ax.scatter(x_1,x_2, dist[0:1000],c=np.linalg.norm([x_1,x_2,dist[0:1000]],axis=0))
+ax.set_xlabel('Random variable $X_1$')
+ax.set_ylabel('Random vriable $X_2$')
+ax.set_zlabel('$p_{X1,X2}(x1,x2)$')
+ax.set_title('Variation of Joint distribution ')
 
-data=np.linspace(-1,1,1000)
-plot7=plt.figure(7)
-fx9=(1/(math.pi* np.sqrt(1-(data**2)/v))*(1/(math.pi* np.sqrt(1-(data**2))/v)))*(1/np.sqrt(v))
-plt.plot(data,fx9)
+fig=plt.figure()
+ax=fig.add_subplot(111, projection = '3d')
+ax.scatter(x_1,x_2,result[0:1000],c=np.linalg.norm([x_1,x_2,result[0:1000]],axis=0))
+ax.set_xlabel('Random variable $X_1$')
+ax.set_ylabel('Random vriable $X_2$')
+ax.set_zlabel('$p_{X1}(x1)*p_{X2}(x2)$')
+ax.set_title('Variation of Joint distribution ')
 
-plt.title('variation of joint PDF $p_{X1,X2}(x1,x2)$')
-plt.xlabel('Range of variation of x1,x2')
-plt.ylabel('$p_{X1,X2}(x1,x2)$')
-plt.grid()
-
-# Probman 6.2.2
-
-# Taking inputs for V and theta
-
-print("Finding the value of the jacobian at a point ")
-v = input(" Give an input for V : ")
-v=np.sqrt(float(v))
-theta = input(' Give an input for theta')
-s_theta = math.sin(float(theta))
-c_theta = math.cos(float(theta))
-
-#Calculating Jacobian Matrix
-
-arr = np.array([[(c_theta/(2*v)), (s_theta/(2*v))], [-v*s_theta, v*c_theta]])
-print("The Jacobian is ")
-print(arr)
