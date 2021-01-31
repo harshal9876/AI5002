1228926c2a6afc31314e426cd782c20551f46888
harshal9876 <63243859+harshal9876@users.noreply.github.com>
2021-01-31 16:34:37 +0530

    Update Assignment_2.py

--- Assignment 2/Assignment_2.py
+++ Assignment 2/Assignment_2.py
@@ -8,6 +8,6 @@ from scipy import stats
 X = stats.binom(50, 0.01) # Declare X to be a binomial random variable
 
 #Calculting respective probabilities
-print("\n Probibility of at least once :",1-X.pmf(0))       # P(X >= 0)
+print("\n Probibility of at least once :",1-X.pmf(0))       # P(X >= 1)
 print("\n Probibility of exactly equal to one :",X.pmf(1))  # P(X = 1)
 print("\n Probibility of at least twice :",1-X.cdf(1))      # P(X >= 2)
