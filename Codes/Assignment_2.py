5a308a6b76494e36615b8b651e35aa1b3f2a1a13
harshal9876 <63243859+harshal9876@users.noreply.github.com>
2021-02-01 11:56:48 +0530

    Create Assignment_2.py

---
+++ Codes/Assignment_2.py
@@ -0,0 +1,14 @@
+  
+#Problem 1.8
+
+#Importing Libraries
+import math
+from scipy import stats
+
+#Defining Binomial Distribution
+X = stats.binom(50, 0.01) # Declare X to be a binomial random variable
+
+#Calculting respective probabilities
+print("\n Probibility of at least once :",1-X.pmf(0))       # P(X >= 1)
+print("\n Probibility of exactly equal to one :",X.pmf(1))  # P(X = 1)
+print("\n Probibility of at least twice :",1-X.cdf(1))      # P(X >= 2)
