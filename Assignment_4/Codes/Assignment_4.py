750bd6332e2dc62290501004ab2877620f9bef01
harshal9876 <63243859+harshal9876@users.noreply.github.com>
2021-02-16 12:00:10 +0530

    Update Assignment_4.py

--- Assignment_4/Codes/Assignment_4.py
+++ Assignment_4/Codes/Assignment_4.py
@@ -13,11 +13,11 @@ def mean_heads(n):
     mean_value = mean_value + (count/len(omega))*i
   return mean_value
   
-  #Mean no of head for a fair coin thrice 
+#Mean no of head for a fair coin thrice 
   
-  print("The mean number of heads in three tosses for a fair coin is : ",mean_heads(3))
+print("The mean number of heads in three tosses for a fair coin is : ",mean_heads(3))
   
-  #Extra : to plot average tosses for different number of tosses for a fair coin
+#Extra : to plot average tosses for different number of tosses for a fair coin
   
 coin_toss_set = []
 mean_head_set = []
@@ -25,7 +25,7 @@ for i in range(1,11):
   mean_head_set.append(mean_heads(i))
   coin_toss_set.append(i)
   
- plt.plot(np.array(coin_toss_set),np.array(mean_head_set),'o:b')
+plt.plot(np.array(coin_toss_set),np.array(mean_head_set),'o:b')
 plt.title("Mean number of heads in coin toss")
 plt.xlabel("Number of coin toss")
 plt.ylabel("Mean number of heads")
