c6a4b21399a713c43f3a9f3862254dfdfba7c8ff
harshal9876 <63243859+harshal9876@users.noreply.github.com>
2021-02-26 11:48:49 +0530

    Update Assignment_5.py

--- Assignment_5/Codes/Assignment_5.py
+++ Assignment_5/Codes/Assignment_5.py
@@ -1,10 +1,33 @@
-import math
-from scipy import stats
+#Importing libraries
+import random
+import numpy as np
+import matplotlib.pyplot as plt 
 
-# Defining Binomial Distribution
-# Declare X to be a binomial random variable
-X = stats.binom(6, 1/6) 
+#defining function count to count the occourance of a particular value in a list 
+def count(list,a):
+  count_val = 0
+  for i in range(len(list)):
+    if list[i]== a:
+      count_val = count_val + 1
+  return count_val
 
-#Calculting respective probabilities
+#generating samples
+six_occourance = []
+for i in range(100000):
+  Six_throw = []
+  for j in range(6):
+    Six_throw.append(random.randint(1,6))  
+  six_occourance.append(count(Six_throw,6))
 
-print("\n Probability of throwing at most 2 sixes in 6 throws of a single die is :",X.cdf(2))      # P(X <= 2)
+  
+ #plotting samples 
+plt.hist(six_occourance,bins = 12 ,align = 'left')
+plt.xlabel("Number of sixes in 6 throws of a die ")
+plt.ylabel("Number of sixes appear")
+plt.title("Occourance of sixes in 1,000,000 consecutive times in 6 throws of a fair die")
+plt.show()
+
+#Result
+probability = (count(histo,1) + count(histo,2) + count(histo,0))/100000
+
+print("The probability for throwing at most two sixes in 6 throws of a single die is : ", probability)
