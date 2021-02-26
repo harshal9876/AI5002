48d278d462f7189089d0de2730184f973deec7b3
harshal9876 <63243859+harshal9876@users.noreply.github.com>
2021-02-26 12:05:52 +0530

    Update Assignment_5.py

--- Assignment_5/Codes/Assignment_5.py
+++ Assignment_5/Codes/Assignment_5.py
@@ -11,7 +11,7 @@ def count(list,a):
       count_val = count_val + 1
   return count_val
 
-#generating samples
+#Generating samples
 six_occourance = []
 for i in range(100000):
   Six_throw = []
@@ -20,14 +20,19 @@ for i in range(100000):
   six_occourance.append(count(Six_throw,6))
 
   
- #plotting samples 
-plt.hist(six_occourance,bins = 12 ,align = 'left')
+#Plotting samples 
+hist,bins = np.histogram(six_occourance,6)
+cdf = hist.cumsum()
+cdf_normalized = cdf * float(hist.max()) / cdf.max()
+plt.plot(cdf_normalized, color = 'r')
+plt.hist(six_occourance, color = 'b',bins = 12 , align = 'left')
+plt.legend(('CDF normalized','Histogram'), loc = 'upper right')
 plt.xlabel("Number of sixes in 6 throws of a die ")
 plt.ylabel("Number of sixes appear")
 plt.title("Occourance of sixes in 1,000,000 consecutive times in 6 throws of a fair die")
 plt.show()
 
 #Result
-probability = (count(histo,1) + count(histo,2) + count(histo,0))/100000
+probability = (count(six_occourance,1) + count(six_occourance,2) + count(six_occourance,0))/100000
 
 print("The probability for throwing at most two sixes in 6 throws of a single die is : ", probability)
